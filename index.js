106b86c66cab62b764b1b051be3021119e6ad70e
lsw6684 <lsw6684@hanmail.net>
2020-11-04 15:10:48 +0900

    installed helmet

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
 const app = express();
 
 const PORT = 4000;
@@ -9,12 +11,9 @@ const handleHome = (req, res) => res.send("Hello from home");
 
 const handleProfile = (req, res) => res.send("You are on my profile");
 
-const betweenHome = (req, res, next) => {
-    console.log("Between");
-    next();
-};
-app.use(betweenHome);
+app.use(helmet());
 
+app.use(morgan("dev"));
 
 app.get("/",handleHome);
 
