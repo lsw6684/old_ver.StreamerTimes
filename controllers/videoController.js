b2acd4d0b9c1d21b63be2c9847ceaaa95882427d
lsw6684 <lsw6684@hanmail.net>
2020-11-19 21:55:02 +0900

    something wrong happens.. I don't know whether I can fix it

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,84 +1,104 @@
 import routes from "../routes";
-import Video from "../models/Video"
+import Video from "../models/Video";
 
-export const home = async(req, res) => {
-  try{
-    const videos = await Video.find({});
+// Home
+
+export const home = async (req, res) => {
+  try {
+    const videos = await Video.find({}).sort({ _id: -1 });
     res.render("home", { pageTitle: "Home", videos });
-  } catch(error){
+  } catch (error) {
     console.log(error);
-    res.render("home", { pageTitle: "Home", videos: []});
+    res.render("home", { pageTitle: "Home", videos: [] });
   }
 };
 
-export const search = (req, res) => {
+// Search
+
+export const search = async (req, res) => {
   const {
     query: { term: searchingBy }
   } = req;
+  let videos = [];
+  try {
+    videos = await Video.find({
+      title: { $regex: searchingBy, $options: "i" }
+    });
+  } catch (error) {
+    console.log(error);
+  }
   res.render("search", { pageTitle: "Search", searchingBy, videos });
 };
+
+// Upload
 
 export const getUpload = (req, res) =>
   res.render("upload", { pageTitle: "Upload" });
 
-export const postUpload = async(req, res) => {
+export const postUpload = async (req, res) => {
   const {
     body: { title, description },
-    file : { path }
+    file: { path }
   } = req;
   const newVideo = await Video.create({
-    fileUrl : path,
+    fileUrl: path,
     title,
     description
-  })
+  });
   res.redirect(routes.videoDetail(newVideo.id));
 };
 
-export const videoDetail = async(req, res) =>{
+// Video Detail
+
+export const videoDetail = async (req, res) => {
   const {
-    params: {id}
+    params: { id }
   } = req;
-  try{
+  try {
     const video = await Video.findById(id);
-  res.render("videoDetail", { pageTitle: video.title, video });
-  } catch(error){
-    console.log(error)
-    res.redirect(route.home);
+    res.render("videoDetail", { pageTitle: video.title, video });
+  } catch (error) {
+    res.redirect(routes.home);
   }
 };
 
-export const getEditVideo = async (req, res) =>{
+// Edit Video
+
+export const getEditVideo = async (req, res) => {
   const {
-    params: {id}
+    params: { id }
   } = req;
-  try{
+  try {
     const video = await Video.findById(id);
-    res.render("editVideo", {pageTitle: `Edit ${video.title}`, video})
-  } catch(error){
+    res.render("editVideo", { pageTitle: `Edit ${video.title}`, video });
+  } catch (error) {
     res.redirect(routes.home);
   }
-}
-export const postEditVideo = async(req, res) => {
+};
+
+export const postEditVideo = async (req, res) => {
   const {
-    params:{id},
-    body: {title, description}
-  } = req;  
-  try{
-    await Video.findOneAndUpdate({_id:id}, {title, description});
+    params: { id },
+    body: { title, description }
+  } = req;
+  try {
+    await Video.findOneAndUpdate({ _id: id }, { title, description });
     res.redirect(routes.videoDetail(id));
-  }catch(error){
+  } catch (error) {
     res.redirect(routes.home);
   }
 };
 
+// Delete Video
 
-
-export const deleteVideo = async(req, res) => {
-  const{
-    params: {id}
+export const deleteVideo = async (req, res) => {
+  const {
+    params: { id }
   } = req;
-  try{
-    await Video.findOneAndRemove({_id: id});
-  } catch(error){}
-    res.redirect(routes.home);
+  try {
+    await Video.findOneAndRemove({ _id: id });
+  } catch (error) {
+    console.log(error);
+  }
+  res.redirect(routes.home);
 };
